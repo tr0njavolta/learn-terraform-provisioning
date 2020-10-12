8e94949702abd7e030fd6002dc0fb3ffc69ba009
Rachel Sharp <rachel@hashicorp.com>
2020-10-11 21:16:14 -0400

    Update main.tf

--- instances/main.tf
+++ instances/main.tf
@@ -1,3 +1,13 @@
+terraform {
+  required_version = ">= 0.13.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "2.69.0"
+    }
+  }
+}
+
 provider "aws" {
   region = var.region
 }
